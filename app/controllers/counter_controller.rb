cc3b181acd81d4ea4e772c294ab592db1aef57d6
shimobayashi <akimasa@shimobayashi.name>
2012-11-17 23:55:27 +0900

    append

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -5,7 +5,12 @@ class CounterController < ApplicationController
   end
 
   def counter
-    img = Magick::Image.read(File.expand_path('app/assets/images/nekomimi/0.gif', Rails.root)).first
+    list = Magick.ImageList.new
+    for i in 0...9
+      img = Magick::Image.read(File.expand_path("app/assets/images/nekomimi/#{i}.gif", Rails.root)).first
+      list.concat(img)
+    end
+    list.append(false)
     self.content_type = img.mime_type
     self.response_body = img.to_blob
   end
