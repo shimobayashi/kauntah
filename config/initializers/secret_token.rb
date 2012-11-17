6f41649c56a5277760e2745bbee85905258d5fa2
shimobayashi <akimasa@shimobayashi.name>
2012-11-17 19:50:51 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kauntah::Application.config.secret_token = 'd8a383134179f276573e7ed4188c2349fa779b9accc823005a28d253a18ba0cfa300876665afe94ca4d160d1625e99de6a69e0efaeb737bb0014bbf712e78e2e'
